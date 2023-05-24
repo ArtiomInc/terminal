02b29565c26914278685b687428b4b7ffab0de82
Artiom Duc <artiom.duc@asyril.com>
2023-05-24 16:05:48 +0200

    Added behaviour

--- src/components/terminal.vue
+++ src/components/terminal.vue
@@ -1,40 +1,120 @@
 <script lang="ts">
+import { ref, provide } from 'vue'
 export default {
   data() {
     return {
-      inputCmd: '',
-      parentMessage: 'Parent',
-      items: [{ message: 'Foo' }, { message: 'Bar' }]
+      test: '<br>',
+      inputCmd: 'Hello',
+      inputLastCmd: 'Hello',
+      userMsg: 'guest',
+      deviceMsg: 'terminal.arduc.ch',
+      items: [],
     }
   },
+  mounted() {
+    this.add();
+    this.focusInput();
+  },
   methods: {
+    focusInput() {
+      this.$refs.command.focus();
+    },
+    scrollToBottom() {
+      const chatContainer = this.$refs.chat;
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+    },
     add() {
-      this.items.push({ message: this.inputCmd })
+      this.items.push({ type: true, message: this.inputCmd });
+      this.sendCmd(this.inputCmd.toLowerCase());
+      this.inputLastCmd = this.inputCmd;
+      this.inputCmd = '';  this.$nextTick(() => {
+        this.scrollToBottom();
+      });
+    },
+    remember() {
+      this.inputCmd = this.inputLastCmd;
+    },
+    sendCmd(cmd) {
+      if (cmd === 'hello') {
+        this.items.push({ type: false, message: ["Type 'help' to see list of available commands.",
+        '--',
+        "The project is from Artiom Duc ❤️ type 'repo' to check out the repository.",
+        '--']});
+      } else if (cmd === 'help') {
+        this.items.push({ type: false, message: ["Avaiable commands:",
+                                                '<strong>hello</strong> - Say hello to terminal',
+                                                '<strong>help</strong> - List all commands',
+                                                '<strong>repo</strong> - Give repository'] });
+      } else if (cmd === 'repo') {
+        this.items.push({ type: false, message: ['The original repo: <a target="_blank" href="https://github.com/ArtiomInc/terminal">https://github.com/ArtiomInc/terminal</a>'] });
+      } else if (cmd === 'test') {
+        this.items.push({ type: false, message: ['<pre>   f    t </pre>'] });
+      } else {
+        this.items.push({ type: false, message: ['unknown command<br>' + "Type 'help' to see list of available commands."] });
+      }
     }
   }
 }
 </script>
 
 <template>
-  <div class="card">
+  <div ref="chat" class="card" @click="focusInput">
     <ul class="terminal">
-      <li v-for="(item, index) in items">
-        <h1>{{ parentMessage }} </h1> - {{ index }} - {{ item.message }}
+      <li v-for="(item) in items">
+        <span v-if="item.type" class="user">{{ userMsg }}</span>
+        <span v-if="item.type" class="at">@</span>
+        <span v-if="item.type" class="device">{{ deviceMsg }}</span>
+        <span v-if="item.type" class="two_points">$:&nbsp;</span>
+        <span v-if="item.type" v-for="(msg) in item.message">{{ msg }}</span>
+        <span v-if="item.type == false" v-for="(msg) in item.message" class="content_msg" v-html="msg"></span>
+      </li>
+      <li ref="li_bottom" class="input">
+        <span class="user">{{ userMsg }}</span>
+        <span class="at">@</span>
+        <span class="device">{{ deviceMsg }}</span>
+        <span class="two_points">$:&nbsp;</span>
+        <input ref="command" v-model="inputCmd" @keydown.enter="add" @keydown.up="remember"/>
       </li>
     </ul>
-    <input v-model="inputCmd" @keyup.enter="add"/><button @click="add">send</button>
   </div>
 </template>
 
 <style scoped>
 .card{
-  height: 100vh;
-  width: 100vw;
-  margin: 0;
-  padding: 20px;
-  border: 5px solid orange;
+  height: calc(100vh - 14px);
+  width: calc(100vw - 14px);
+  margin: 7px;
+  border: 2px solid #d79921;
+  border-radius: 5px;
+  overflow: auto;
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+}
+.card::-webkit-scrollbar { /* Chrome, Safari and Opera */
+  display: none;
 }
 .terminal {
   list-style-type: none;
+  margin: 20px
+}
+.content_msg {
+  display: block;
+  margin: 0;
+  padding: 0;
+}
+.user{
+  color: #8ae234;
+}
+.device{
+  color: #6792c7;
+}
+.input{
+  display: flex;
+}
+input {
+  background-color: transparent;
+  width: 100%;
+  border: 0;
+  outline: 0;
 }
 </style>
