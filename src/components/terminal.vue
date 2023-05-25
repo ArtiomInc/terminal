76d0ac6fb8e4f6b84c385d78dcd4ce2417bff627
Artiom Duc <artiom.game@gmail.com>
2023-05-25 20:22:59 +0200

    Adde Favicon and change color

--- src/components/terminal.vue
+++ src/components/terminal.vue
@@ -2,11 +2,10 @@
 export default {
   data() {
     return {
-      test: '<br>',
       inputCmd: 'Hello',
       inputLastCmd: 'Hello',
       userMsg: 'guest',
-      deviceMsg: 'terminal.arduc.ch',
+      deviceMsg: 'term.arduc.ch',
       items: [],
     }
   },
@@ -35,21 +34,40 @@ export default {
     },
     sendCmd(cmd) {
       if (cmd === 'hello') {
-        this.items.push({ type: false, message: ["Type 'help' to see list of available commands.",
+        this.items.push({ type: false, message: ["Welcome on <strong>Artiom</strong> terminal website !",
+        "Type 'help' to see list of available commands.",
         '--',
-        "The project is from Artiom Duc ❤️ type 'repo' to check out the repository.",
+        "The project is created by Artiom Duc / Design inspired from M4TT72",
         '--']});
       } else if (cmd === 'help') {
         this.items.push({ type: false, message: ["Avaiable commands:",
-                                                '<strong>hello</strong> - Say hello to terminal',
-                                                '<strong>help</strong> - List all commands',
-                                                '<strong>repo</strong> - Give repository'] });
+        '<strong>hello</strong>',
+        '<strong>help</strong>',
+        '<strong>sudo</strong>',
+        '<strong>date</strong>',
+        '<strong>time</strong>',
+        '<strong>weather</strong>',
+        '<strong>about</strong>',
+        '<strong>linkedin</strong>',
+        '<strong>repo</strong>'] });
       } else if (cmd === 'repo') {
-        this.items.push({ type: false, message: ['The original repo: <a target="_blank" href="https://github.com/ArtiomInc/terminal">https://github.com/ArtiomInc/terminal</a>'] });
-      } else if (cmd === 'test') {
-        this.items.push({ type: false, message: ['<pre>   f    t </pre>'] });
+        this.items.push({ type: false, message: ['The repository: <a target="_blank" href="https://github.com/ArtiomInc/terminal">https://github.com/ArtiomInc/terminal</a>'] });
+      } else if (cmd === "sudo") {
+        this.items.push({ type: false, message: ['<pre>You are now logged in as root <strong>(wip)</strong></pre>'] });
+      } else if (cmd === "date") {
+        this.items.push({ type: false, message: ['<pre>Command not available yet: <strong>work in progress...</strong></pre>'] });
+      } else if (cmd === "time") {
+        this.items.push({ type: false, message: ['<pre>Command not available yet: <strong>work in progress...</strong></pre>'] });
+      } else if (cmd === "weather") {
+        this.items.push({ type: false, message: ['<pre>Command not available yet: <strong>work in progress...</strong></pre>'] });
+      } else if (cmd === "about") {
+        this.items.push({ type: false, message: ['<pre>Command not available yet: <strong>work in progress...</strong></pre>'] });
+      } else if (cmd === "linkedin") {
+        this.items.push({ type: false, message: ['<pre>Command not available yet: <strong>work in progress...</strong></pre>'] });
+      } else if (cmd === "secret") {
+        this.items.push({ type: false, message: ['<pre>Command not available yet: <strong>work in progress...</strong></pre>'] });
       } else {
-        this.items.push({ type: false, message: ['unknown command<br>' + "Type 'help' to see list of available commands."] });
+        this.items.push({ type: false, message: ['Command not found: ' + "Type 'help' to see list of available commands."] });
       }
     }
   }
@@ -59,13 +77,13 @@ export default {
 <template>
   <div ref="chat" class="card" @click="focusInput">
     <ul class="terminal">
-      <li v-for="(item) in items">
+      <li v-for="(item) in items" v-bind:key="item">
         <span v-if="item.type" class="user">{{ userMsg }}</span>
         <span v-if="item.type" class="at">@</span>
         <span v-if="item.type" class="device">{{ deviceMsg }}</span>
         <span v-if="item.type" class="two_points">$:&nbsp;</span>
-        <span v-if="item.type" v-for="(msg) in item.message">{{ msg }}</span>
-        <span v-if="item.type == false" v-for="(msg) in item.message" class="content_msg" v-html="msg"></span>
+        <span v-if="item.type" v-for="msg in item.message">{{ msg }}</span>
+        <span v-if="item.type == false" v-for="msg in item.message" class="content_msg" v-html="msg"></span>
       </li>
       <li ref="li_bottom" class="input">
         <span class="user">{{ userMsg }}</span>
@@ -83,7 +101,7 @@ export default {
   height: calc(100vh - 14px);
   width: calc(100vw - 14px);
   margin: 7px;
-  border: 2px solid #d79921;
+  border: 2px solid #00DC82;
   border-radius: 5px;
   overflow: auto;
   -ms-overflow-style: none;  /* IE and Edge */
@@ -102,7 +120,7 @@ export default {
   padding: 0;
 }
 .user{
-  color: #8ae234;
+  color: #00DC82;
 }
 .device{
   color: #6792c7;
